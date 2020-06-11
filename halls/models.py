1f27e0c9c889642eef083fe7f694a4c936b31370
Robert Villazana <r.villazana@yahoo.com>
2020-06-11 13:17:45 -0500

    first commit... with some stuff...

---
+++ halls/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Hall(models.Model):
+    title = models.CharField(max_length=255)
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    youtube_id = models.CharField(max_length=255)
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
