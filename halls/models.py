7aec9f8005687a7389dba30096d9b2f47291c2df
Robert Villazana <r.villazana@yahoo.com>
2020-06-12 12:05:26 -0500

    added more components and html templates

--- halls/models.py
+++ halls/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Hall(models.Model):
     title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Video(models.Model):
     title = models.CharField(max_length=255)
--- halls/views.py
+++ halls/views.py
@@ -1,19 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
-from .models import Hall
+from django.contrib.auth import authenticate, login
+from .models import Hall, Video
+from .forms import VideoForm, SearchForm
 
 def home(request):
     return render(request, 'halls/home.html')
+
+def dashboard(request):
+    return render(request, 'halls/dashboard.html')
+
+def add_video(request, pk):
+    form = VideoForm()
+
+    search_form = SearchForm()
+
+    if request.method == 'POST':
+        filled_form = VideoForm(request.POST)
+        if filled_form.is_valid():
+            video = Video()
+            video.url = filled_form.cleaned_data['url']
+            video.title = filled_form.cleaned_data['title']
+            video.youtube_id = filled_form.cleaned_data['youtube_id']
+            video.hall = Hall.objects.get(pk=pk)
+            video.save()
+            
+    return render(request, 'halls/add_video.html', {'form':form, 'search_form':search_form})
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        # lines 19-21 login user immediately after signing up
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
 class CreateHall(generic.CreateView):
     model = Hall
     fields = ['title']
     template_name = 'halls/create_hall.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('dashboard')
+
+class DetailHall(generic.DetailView):
+    model = Hall
+    template_name = 'halls/detail_hall.html'
+
+class UpdateHall(generic.UpdateView):
+    model = Hall
+    template_name = 'halls/update_hall.html'
+    fields = ['title']
+    success_url = reverse_lazy('dashboard')
+
+class DeleteHall(generic.DeleteView):
+    model = Hall
+    template_name = 'halls/delete_hall.html'
+    success_url = reverse_lazy('dashboard')    
