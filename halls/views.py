a1a64441e49e2c0180aee2e2cbd0177735959072
Robert Villazana <r.villazana@yahoo.com>
2020-06-15 14:37:18 -0500

    added delete video functionality

--- halls/views.py
+++ halls/views.py
@@ -57,6 +57,11 @@ def video_search(request):
         return JsonResponse(response.json())
     return JsonResponse({'error':'Not able to validate form'})
 
+class DeleteVideo(generic.DeleteView):
+    model = Video
+    template_name = 'halls/delete_video.html'
+    success_url = reverse_lazy('dashboard')   
+
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
